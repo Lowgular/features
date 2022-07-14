6fb01d46fb188377267e2189ea09016acc1e6866
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-07-14 22:39:20 +0200

    feat/ firebase installed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'lowgular-features',
+    appId: '1:948846251352:web:c96e65891063c9d219692d',
+    databaseURL: 'https://lowgular-features-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'lowgular-features.appspot.com',
+    locationId: 'europe-central2',
+    apiKey: 'AIzaSyAAUYz0ORKU-mg0fwUnjBiqHqHh7sfw2ww',
+    authDomain: 'lowgular-features.firebaseapp.com',
+    messagingSenderId: '948846251352',
+  },
   production: true
 };
