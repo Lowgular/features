a3970fb9e9a9aa1850692516faf796d2c88929b5
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-07-22 11:02:20 +0200

    storage and state bugs fixed

--- projects/feature/src/lib/adapters/secondary/storages/in-memory-feature-id.storage.ts
+++ projects/feature/src/lib/adapters/secondary/storages/in-memory-feature-id.storage.ts
@@ -4,7 +4,6 @@ import { map } from 'rxjs/operators';
 import { SetsStateFeatureIdContextPort } from '../../../application/ports/secondary/context/sets-state-feature-id.context-port';
 import { SelectsFeatureIdContextPort } from '../../../application/ports/secondary/context/selects-feature-id.context-port';
 import { FeatureIdContext } from '../../../application/ports/secondary/context/feature-id.context';
-import { SetFeatureIdCommand } from '../../../application/ports/primary/command/set-feature-id.command';
 
 @Injectable()
 export class InMemoryFeatureIdStorage
@@ -14,8 +13,10 @@ export class InMemoryFeatureIdStorage
     Partial<FeatureIdContext>
   >({});
 
-  setFeatureId(command: SetFeatureIdCommand): Observable<void> {
-    return of(this._subject.next(command)).pipe(map(() => void 0));
+  setFeatureId(selectedFeatureId: string): Observable<void> {
+    return of(this._subject.next({ selectedFeatureId })).pipe(
+      map(() => void 0)
+    );
   }
 
   select(): Observable<Partial<FeatureIdContext>> {
