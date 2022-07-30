548d27824664d5b452709dc099ce7e8bb200ea9f
Magda Ramotowska <rtkmagdalena@gmail.com>
2022-07-30 21:28:15 +0200

    feat/ different colour for bug and feature

--- projects/feature/src/lib/application/ports/primary/query/feature-list-item.query.ts
+++ projects/feature/src/lib/application/ports/primary/query/feature-list-item.query.ts
@@ -1,5 +1,7 @@
 export class FeatureListItemQuery {
   public readonly voters: number;
+  public readonly isBug: boolean;
+
   constructor(
     public readonly title: string,
     public readonly description: string,
@@ -9,5 +11,6 @@ export class FeatureListItemQuery {
     private readonly _voters: string[]
   ) {
     this.voters = [...new Set(this._voters)].length;
+    this.isBug = this.type === 'bug' ? true : false;
   }
 }
